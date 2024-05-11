abe2202deefc53beb69233aef11b782373f46c53
marcoshssilva <76234459+marcoshssilva@users.noreply.github.com>
2024-05-10 22:04:11 -0300

    Startup project

---
+++ src/main/java/br/com/marcoshssilva/springbooteureka/SpringBootEurekaApplication.java
@@ -0,0 +1,13 @@
+package br.com.marcoshssilva.springbooteureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEurekaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/marcoshssilva/springbooteureka/SpringBootEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcoshssilva.springbooteureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
