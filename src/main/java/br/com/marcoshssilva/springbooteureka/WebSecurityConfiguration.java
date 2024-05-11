2e29777e98c76cd3c7bf0fc6d64cf0f012cd6b34
marcoshssilva <76234459+marcoshssilva@users.noreply.github.com>
2024-05-11 00:08:19 -0300

    feat: Setup CORS with full enabled to anyone on any path

--- src/main/java/br/com/marcoshssilva/springbooteureka/WebSecurityConfiguration.java
+++ src/main/java/br/com/marcoshssilva/springbooteureka/WebSecurityConfiguration.java
@@ -9,6 +9,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableMethodSecurity()
@@ -18,7 +23,8 @@ public class WebSecurityConfiguration {
     static final String[] AUTH_WHITELIST = { "/actuator/**" };
 
     @Bean
-    SecurityFilterChain securityFilterChainConfigure(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChainConfigure(HttpSecurity http) throws Exception {
+        http.cors(corsConfigurer -> corsConfigurer.configurationSource(corsConfigurationSource()));
         http.csrf(CsrfConfigurer::disable);
         http.sessionManagement(sessionConfigurer -> sessionConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         http.authorizeHttpRequests(httpRequestsConfigurer -> httpRequestsConfigurer.requestMatchers(AUTH_WHITELIST).permitAll().anyRequest().authenticated());
@@ -26,4 +32,14 @@ public class WebSecurityConfiguration {
         return http.build();
     }
 
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedMethods(List.of("*"));
+        configuration.setAllowedHeaders(List.of("*"));
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", configuration);
+        return urlBasedCorsConfigurationSource;
+    }
 }
