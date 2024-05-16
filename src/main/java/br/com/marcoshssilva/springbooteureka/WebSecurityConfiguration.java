c422d8cdac27196296226621c25b0f2f91c2f509
SenhorDasEstrelas443 <76234459+marcoshssilva@users.noreply.github.com>
2024-05-15 21:54:31 -0300

    feat: review WebSecurity

--- src/main/java/br/com/marcoshssilva/springbooteureka/WebSecurityConfiguration.java
+++ src/main/java/br/com/marcoshssilva/springbooteureka/WebSecurityConfiguration.java
@@ -9,11 +9,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.List;
 
 @Configuration
 @EnableMethodSecurity()
@@ -26,28 +21,11 @@ public class WebSecurityConfiguration {
 
     @Bean
     public SecurityFilterChain securityFilterChainConfigure(HttpSecurity http) throws Exception {
-        log.info("Configured with enabled Cross-Origin-Resource-Sharing");
-        http.cors(corsConfigurer -> corsConfigurer.configurationSource(corsConfigurationSource()));
-        log.info("Configured with disabled Csrf Tokens");
         http.csrf(CsrfConfigurer::disable);
-        log.info("Configured with disabled Sessions");
         http.sessionManagement(sessionConfigurer -> sessionConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-        log.info("Configured with enabled path only to authenticated users");
         http.authorizeHttpRequests(httpRequestsConfigurer -> httpRequestsConfigurer.requestMatchers(AUTH_WHITELIST).permitAll().anyRequest().authenticated());
-        log.info("Configured with enabled authentication using Http Basic Auth");
         http.httpBasic(Customizer.withDefaults());
         return http.build();
     }
 
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        log.info("Setup cors with fully enabled Origins, Methods and Headers");
-        configuration.setAllowedOrigins(List.of("*"));
-        configuration.setAllowedMethods(List.of("*"));
-        configuration.setAllowedHeaders(List.of("*"));
-        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
-        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", configuration);
-        return urlBasedCorsConfigurationSource;
-    }
 }
