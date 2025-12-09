a97afaee4f3843fc2eadd75f17682739a0329806
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2025-12-08 21:12:53 -0300

    feat: Create controller AdminController

---
+++ src/main/java/br/com/marcoshssilva/springbooteureka/controller/AdminController.java
@@ -0,0 +1,6 @@
+package br.com.marcoshssilva.springbooteureka.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/api/admin")
+public class AdminController { }
