b7ee5fd12b87c4a115a655d43ac5d57343d9c389
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2025-12-13 12:25:59 -0300

    feat: Enable SchedulerConfiguration

---
+++ src/main/java/br/com/marcoshssilva/springbooteureka/configs/SchedulerConfiguration.java
@@ -0,0 +1,21 @@
+package br.com.marcoshssilva.springbooteureka.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+@EnableAsync
+@EnableScheduling
+public class SchedulerConfiguration {
+    @Bean
+    ThreadPoolTaskExecutor threadPoolTaskExecutor() {
+        ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+        taskExecutor.setCorePoolSize(5);
+        taskExecutor.setMaxPoolSize(10);
+        taskExecutor.setQueueCapacity(25);
+        return taskExecutor;
+    }
+}
