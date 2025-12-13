8eac66897dbd8234d20f4cec713afcb6e4ac75a6
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2025-12-13 12:28:57 -0300

    feat: call tasks after application startup

--- src/main/java/br/com/marcoshssilva/springbooteureka/SpringBootEurekaApplication.java
+++ src/main/java/br/com/marcoshssilva/springbooteureka/SpringBootEurekaApplication.java
@@ -1,15 +1,26 @@
 package br.com.marcoshssilva.springbooteureka;
 
+import br.com.marcoshssilva.springbooteureka.domain.tasks.InitMetricsUserIfNotExistsTask;
+import br.com.marcoshssilva.springbooteureka.domain.tasks.InitSuperUserIfNotExistsTask;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@lombok.RequiredArgsConstructor
 @EnableEurekaServer
 @SpringBootApplication
-public class SpringBootEurekaApplication {
+public class SpringBootEurekaApplication implements CommandLineRunner {
+	private final InitSuperUserIfNotExistsTask initSuperUserIfNotExistsTask;
+	private final InitMetricsUserIfNotExistsTask initMetricsUserIfNotExistsTask;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootEurekaApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		initSuperUserIfNotExistsTask.executeTask();
+		initMetricsUserIfNotExistsTask.executeTask();
+	}
 }
