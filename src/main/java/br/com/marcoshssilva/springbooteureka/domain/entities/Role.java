8454217dae90b9474991d74300963f92f2e61840
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2025-12-13 09:57:07 -0300

    feat: rename Authority.java to Role.java

--- src/main/java/br/com/marcoshssilva/springbooteureka/domain/entities/Role.java
+++ src/main/java/br/com/marcoshssilva/springbooteureka/domain/entities/Role.java
@@ -11,7 +11,7 @@ import java.util.Objects;
 @lombok.Data
 @Entity
 @Table(name = "authorities")
-public class Authority {
+public class Role {
     @Id
     private String username;
     private String authority;
@@ -20,7 +20,7 @@ public class Authority {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        Authority authority1 = (Authority) o;
+        Role authority1 = (Role) o;
         return Objects.equals(username, authority1.username) && Objects.equals(authority, authority1.authority);
     }
 
