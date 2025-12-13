87332a02565f4e660ad97b7fa5e05f152d02f8e4
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2025-12-13 12:28:34 -0300

    feat: create tasks to create users metrics and admin if not exists

---
+++ src/main/java/br/com/marcoshssilva/springbooteureka/domain/tasks/InitMetricsUserIfNotExistsTask.java
@@ -0,0 +1,39 @@
+package br.com.marcoshssilva.springbooteureka.domain.tasks;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.CompletableFuture;
+
+@lombok.RequiredArgsConstructor
+@lombok.extern.slf4j.Slf4j
+@Component
+public class InitMetricsUserIfNotExistsTask {
+    private static final String DEFAULT_ROLE = "METRICS";
+
+    @Value("${eureka.instance.metadata-map.username}")
+    String metricsUsername = "metrics";
+
+    @Value("${eureka.instance.metadata-map.password}")
+    String metricsPassword = "changeit";
+
+    private final UserDetailsManager userDetailsManager;
+    private final PasswordEncoder passwordEncoder;
+
+    @Async
+    public CompletableFuture<Void> executeTask() {
+        log.info("Checking if user {} exists in database.", metricsUsername);
+        if (!userDetailsManager.userExists(metricsUsername)) {
+            log.info("User {} not found in database. Creating...", metricsUsername);
+            userDetailsManager.createUser(User.builder().username(metricsUsername).password(passwordEncoder.encode(metricsPassword)).roles(DEFAULT_ROLE).build());
+            log.info("User {} created succesfully.", metricsUsername);
+        } else {
+            log.info("Skip create user {}. Already exists in database.", metricsUsername);
+        }
+        return CompletableFuture.completedFuture(null);
+    }
+}
---
+++ src/main/java/br/com/marcoshssilva/springbooteureka/domain/tasks/InitSuperUserIfNotExistsTask.java
@@ -0,0 +1,33 @@
+package br.com.marcoshssilva.springbooteureka.domain.tasks;
+
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.CompletableFuture;
+
+@lombok.RequiredArgsConstructor
+@lombok.extern.slf4j.Slf4j
+@Component
+public class InitSuperUserIfNotExistsTask {
+    private static final String DEFAULT_HASH_PASSWORD = "$100801$9aSNEKS2azINKKadHUU5sw==$yA18DF/L2PnKPplJhMLm5Z7JhAT2TAQPq0PipdxjDxU=";
+    private static final String DEFAULT_USERNAME = "admin";
+    private static final String DEFAULT_ROLE = "ADMIN";
+
+
+    private final UserDetailsManager userDetailsManager;
+
+    @Async
+    public CompletableFuture<Void> executeTask() {
+        log.info("Checking if default admin exists in database.");
+        if (!userDetailsManager.userExists(DEFAULT_USERNAME)) {
+            log.info("User {} not found in database. Creating...", DEFAULT_USERNAME);
+            userDetailsManager.createUser(User.builder().username(DEFAULT_USERNAME).password(DEFAULT_HASH_PASSWORD).roles(DEFAULT_ROLE).build());
+            log.info("User {} created succesfully.", DEFAULT_USERNAME);
+        } else {
+            log.info("Skip create default admin. Already exists in database.");
+        }
+        return CompletableFuture.completedFuture(null);
+    }
+}
