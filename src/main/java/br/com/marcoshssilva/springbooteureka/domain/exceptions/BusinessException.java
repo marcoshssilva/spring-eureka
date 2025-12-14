a181cbba9330a710114907f448fc47dcbe021104
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2025-12-14 20:11:57 -0300

    feat: Create BusinessException.java

---
+++ src/main/java/br/com/marcoshssilva/springbooteureka/domain/exceptions/BusinessException.java
@@ -0,0 +1,11 @@
+package br.com.marcoshssilva.springbooteureka.domain.exceptions;
+
+public class BusinessException extends Exception {
+    public BusinessException(String message) {
+        super(message);
+    }
+
+    public BusinessException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
