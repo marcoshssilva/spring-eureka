3c73d80521fe02c031867d5d644204c7b11c0fc8
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2025-12-14 20:57:36 -0300

    feat: implements default responses for Message converter or Validation errors

---
+++ src/main/java/br/com/marcoshssilva/springbooteureka/controller/data/responses/ErrorStatusResponseBodyDto.java
@@ -0,0 +1,45 @@
+package br.com.marcoshssilva.springbooteureka.controller.data.responses;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Objects;
+
+public record ErrorStatusResponseBodyDto(String message, Collection<ErrorField> errors, String status) implements Serializable {
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ErrorStatusResponseBodyDto that = (ErrorStatusResponseBodyDto) o;
+        return Objects.equals(message, that.message) && Objects.equals(errors, that.errors) && Objects.equals(status, that.status);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(message, errors, status);
+    }
+
+    @Override
+    public String toString() {
+        return "ErrorStatusResponseBodyDto{" +
+                "message='" + message + '\'' +
+                ", errors=" + errors +
+                ", status='" + status + '\'' +
+                '}';
+    }
+
+    public record ErrorField(String field, String message, Object receivedData) {
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            ErrorField that = (ErrorField) o;
+            return Objects.equals(field, that.field) && Objects.equals(message, that.message) && Objects.equals(receivedData, that.receivedData);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(field, message, receivedData);
+        }
+    }
+
+}
--- src/main/java/br/com/marcoshssilva/springbooteureka/controller/error/ErrorControlerAdvice.java
+++ src/main/java/br/com/marcoshssilva/springbooteureka/controller/error/ErrorControlerAdvice.java
@@ -1,11 +1,17 @@
 package br.com.marcoshssilva.springbooteureka.controller.error;
 
+import br.com.marcoshssilva.springbooteureka.controller.data.responses.ErrorStatusResponseBodyDto;
+import br.com.marcoshssilva.springbooteureka.controller.data.responses.SimpleStatusResponseBodyDto;
 import br.com.marcoshssilva.springbooteureka.controller.exceptions.BadRequestException;
 import br.com.marcoshssilva.springbooteureka.controller.exceptions.InternalServerErrorException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageConversionException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class ErrorControlerAdvice {
@@ -17,5 +23,16 @@ public class ErrorControlerAdvice {
     @ExceptionHandler(BadRequestException.class)
     public ResponseEntity<Object> badRequestExceptionResolver(BadRequestException e) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getResponseBody());
+    }
+
+    @ExceptionHandler(HttpMessageConversionException.class)
+    public ResponseEntity<Object> httpMessageConversionExceptionResolver(HttpMessageConversionException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new SimpleStatusResponseBodyDto("Invalid or corrupted payload", "ERROR"));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> validationExceptionResolver(MethodArgumentNotValidException e) {
+        ErrorStatusResponseBodyDto err = new ErrorStatusResponseBodyDto("Invalid argument data in payload.", e.getFieldErrors().stream().map(fieldError -> new ErrorStatusResponseBodyDto.ErrorField(fieldError.getField(), fieldError.getDefaultMessage(), fieldError.getRejectedValue())).collect(Collectors.toSet()), "ERROR");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
     }
 }
