e0369a3751102c771fe92213ebef45a0fa30dbb5
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2025-12-14 21:27:59 -0300

    tests: Implements default tests to init application

--- src/test/java/br/com/marcoshssilva/springbooteureka/SpringBootEurekaApplicationTests.java
+++ src/test/java/br/com/marcoshssilva/springbooteureka/SpringBootEurekaApplicationTests.java
@@ -3,11 +3,15 @@ package br.com.marcoshssilva.springbooteureka;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+@lombok.extern.slf4j.Slf4j
 @SpringBootTest
 class SpringBootEurekaApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertDoesNotThrow(() -> {log.info("Application startup with SUCCESS"); });
 	}
 
 }
