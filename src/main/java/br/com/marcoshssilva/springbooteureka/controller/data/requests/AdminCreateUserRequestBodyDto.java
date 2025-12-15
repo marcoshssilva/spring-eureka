bbeca6628892062254010a725b5c8810895ae5ae
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2025-12-14 21:11:10 -0300

    feat: Expose endpoint create-user

---
+++ src/main/java/br/com/marcoshssilva/springbooteureka/controller/data/requests/AdminCreateUserRequestBodyDto.java
@@ -0,0 +1,34 @@
+package br.com.marcoshssilva.springbooteureka.controller.data.requests;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+public record AdminCreateUserRequestBodyDto(@NotBlank String username, @NotBlank String password, @NotNull Boolean enabled, @NotNull String[] roles) {
+    @Override
+    public String toString() {
+        return "AdminCreateUserRequestBodyDto{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", enabled=" + enabled +
+                ", roles=" + Arrays.toString(roles) +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AdminCreateUserRequestBodyDto that = (AdminCreateUserRequestBodyDto) o;
+        return Objects.equals(username, that.username) && Objects.equals(password, that.password) && Objects.equals(enabled, that.enabled) && Arrays.equals(roles, that.roles);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(username, password, enabled);
+        result = 31 * result + Arrays.hashCode(roles);
+        return result;
+    }
+}
--- src/main/java/br/com/marcoshssilva/springbooteureka/controller/endpoints/AdminController.java
+++ src/main/java/br/com/marcoshssilva/springbooteureka/controller/endpoints/AdminController.java
@@ -1,10 +1,13 @@
 package br.com.marcoshssilva.springbooteureka.controller.endpoints;
 
+import br.com.marcoshssilva.springbooteureka.controller.data.requests.AdminCreateUserRequestBodyDto;
 import br.com.marcoshssilva.springbooteureka.controller.exceptions.BadRequestException;
 import br.com.marcoshssilva.springbooteureka.controller.exceptions.InternalServerErrorException;
+import br.com.marcoshssilva.springbooteureka.domain.exceptions.BusinessException;
 import br.com.marcoshssilva.springbooteureka.domain.services.UserControllerService;
 import br.com.marcoshssilva.springbooteureka.controller.data.requests.AdminUpdatePasswordRequestBodyDto;
 import br.com.marcoshssilva.springbooteureka.controller.data.responses.SimpleStatusResponseBodyDto;
+import jakarta.validation.Valid;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,6 +28,19 @@ public class AdminController {
             userControllerService.resetPasswordFromUsername(body.username(), body.newPassword());
             return new SimpleStatusResponseBodyDto("Password has been changed.", "SUCCESS");
         } catch (UsernameNotFoundException e) {
+            throw new BadRequestException(new SimpleStatusResponseBodyDto(e.getMessage(), "ERROR"));
+        } catch (Exception e) {
+            throw new InternalServerErrorException(new SimpleStatusResponseBodyDto("Internal server error", "ERROR"));
+        }
+    }
+
+    @Transactional
+    @PostMapping("/create-user")
+    SimpleStatusResponseBodyDto createUser(@RequestBody @Valid AdminCreateUserRequestBodyDto body) {
+        try {
+            userControllerService.createUser(body.username(), body.password(), body.enabled(), body.roles());
+            return new SimpleStatusResponseBodyDto("User created with success", "SUCCESS");
+        } catch (BusinessException e) {
             throw new BadRequestException(new SimpleStatusResponseBodyDto(e.getMessage(), "ERROR"));
         } catch (Exception e) {
             throw new InternalServerErrorException(new SimpleStatusResponseBodyDto("Internal server error", "ERROR"));
