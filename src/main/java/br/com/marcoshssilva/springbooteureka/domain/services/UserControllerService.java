4c4338b3b8be0f9b862667b981a91da1b3421882
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2025-12-14 21:10:21 -0300

    feat: Implements method createUser in UserControllerService

--- src/main/java/br/com/marcoshssilva/springbooteureka/domain/services/UserControllerService.java
+++ src/main/java/br/com/marcoshssilva/springbooteureka/domain/services/UserControllerService.java
@@ -1,8 +1,11 @@
 package br.com.marcoshssilva.springbooteureka.domain.services;
 
+import br.com.marcoshssilva.springbooteureka.domain.entities.User;
+import br.com.marcoshssilva.springbooteureka.domain.exceptions.BusinessException;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface UserControllerService {
     void resetPasswordFromUsername(final String username, final String newPassword);
+    User createUser(final String username, final String password, Boolean enabled, String[] roles) throws BusinessException;
 }
--- src/main/java/br/com/marcoshssilva/springbooteureka/domain/services/impl/UserControllerServiceImpl.java
+++ src/main/java/br/com/marcoshssilva/springbooteureka/domain/services/impl/UserControllerServiceImpl.java
@@ -1,6 +1,10 @@
 package br.com.marcoshssilva.springbooteureka.domain.services.impl;
 
+import br.com.marcoshssilva.springbooteureka.domain.entities.Role;
+import br.com.marcoshssilva.springbooteureka.domain.entities.RolePK;
 import br.com.marcoshssilva.springbooteureka.domain.entities.User;
+import br.com.marcoshssilva.springbooteureka.domain.exceptions.BusinessException;
+import br.com.marcoshssilva.springbooteureka.domain.repositories.RoleRepository;
 import br.com.marcoshssilva.springbooteureka.domain.repositories.UserRepository;
 import br.com.marcoshssilva.springbooteureka.domain.services.UserControllerService;
 import org.springframework.context.annotation.Primary;
@@ -8,11 +12,15 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+import java.util.stream.Stream;
+
 @Primary
 @Service
 @lombok.RequiredArgsConstructor
 public class UserControllerServiceImpl implements UserControllerService {
     private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
     private final PasswordEncoder passwordEncoder;
 
     @Override
@@ -20,5 +28,19 @@ public class UserControllerServiceImpl implements UserControllerService {
         User user = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("Username not found in database"));
         user.setPassword(passwordEncoder.encode(newPassword));
         userRepository.save(user);
+    }
+
+    @Override
+    public User createUser(String username, String password, Boolean enabled, String[] roles) throws BusinessException {
+        Optional<User> usernameSearch = userRepository.findByUsername(username);
+        if (usernameSearch.isPresent()) {
+            throw new BusinessException("Username already exists in database.");
+        }
+
+        User user = new User(username, passwordEncoder.encode(password), enabled);
+        userRepository.save(user);
+        Stream.of(roles).forEach(role -> roleRepository.save(new Role(new RolePK(user.getUsername(), role))));
+
+        return user;
     }
 }
