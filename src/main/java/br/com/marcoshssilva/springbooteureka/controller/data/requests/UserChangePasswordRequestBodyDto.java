1bdef14df6c635d088588e7d0e7c22acbe21b5c2
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2025-12-19 10:26:34 -0300

    feat: Update password validation constraints in DTO

--- src/main/java/br/com/marcoshssilva/springbooteureka/controller/data/requests/UserChangePasswordRequestBodyDto.java
+++ src/main/java/br/com/marcoshssilva/springbooteureka/controller/data/requests/UserChangePasswordRequestBodyDto.java
@@ -9,13 +9,13 @@ import java.util.Objects;
 
 public record UserChangePasswordRequestBodyDto(
         @NotBlank(message = "Field cannot be null or empty")
-        @Size(min = 8, max = 64, message = "Must contain at least 8 characters and max 64")
-        @Pattern(regexp = "^\\S{8,64}$", message = "Must contains only letters, numbers or special characters, except space")
+        @Size(min = 5, max = 64, message = "Must contain at least 5 characters and max 64")
+        @Pattern(regexp = "^\\S{1,64}$", message = "Must contains only letters, numbers or special characters, except space")
         String newPassword,
 
         @NotBlank(message = "Field cannot be null or empty")
         @Size(min = 5, max = 64, message = "Must contain at least 5 characters and max 64")
-        @Pattern(regexp = "^\\S{5,64}$", message = "Must contains only letters, numbers or special characters, except space")
+        @Pattern(regexp = "^\\S{1,64}$", message = "Must contains only letters, numbers or special characters, except space")
         String oldPassword
 
 ) implements Serializable {
