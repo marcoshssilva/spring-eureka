664c3450567743eb65cbda20ad8d38f1f33b4c06
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2026-07-21 17:01:01 -0300

    Refactor: Rename logger variable, log request details in TracingHeaderFilter

--- src/main/java/br/com/marcoshssilva/springbooteureka/controller/filters/TracingHeaderFilter.java
+++ src/main/java/br/com/marcoshssilva/springbooteureka/controller/filters/TracingHeaderFilter.java
@@ -14,12 +14,13 @@ import java.io.IOException;
 
 @Component
 public class TracingHeaderFilter extends OncePerRequestFilter {
-    private static final Logger log = LoggerFactory.getLogger(TracingHeaderFilter.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(TracingHeaderFilter.class);
     private static final String HEADER_TRACING = "X-Trace-ID";
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         response.setHeader(HEADER_TRACING, Span.current().getSpanContext().getTraceId());
+        LOGGER.info("Request {}?{}", request.getPathTranslated(), request.getQueryString());
         filterChain.doFilter(request, response);
     }
 }
