bd7f68a9fe7dcf8e18f0b814db882bfa1eb6987e
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2026-07-21 17:01:08 -0300

    test: Fix null comparison order in AdminCreateUserRequestBodyDtoTests

--- src/test/java/br/com/marcoshssilva/springbooteureka/controller/data/requests/AdminCreateUserRequestBodyDtoTests.java
+++ src/test/java/br/com/marcoshssilva/springbooteureka/controller/data/requests/AdminCreateUserRequestBodyDtoTests.java
@@ -28,7 +28,7 @@ class AdminCreateUserRequestBodyDtoTests {
         assertEquals(d1, d2);
         assertEquals(d1.hashCode(), d2.hashCode());
         assertNotEquals(d1, d3);
-        assertNotEquals(d1, null);
+        assertNotEquals(null, d1);
     }
 
     @Test
